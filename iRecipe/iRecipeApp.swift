fc20ebca5e16d32dba0568b341bf27599bc1e233
Krzysztof Kozyra <krzysztof.kozyra@altconnect.pl>
2024-04-19 18:20:01 +0200

    Initial Commit

---
+++ iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by Krzysztof Kozyra on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
